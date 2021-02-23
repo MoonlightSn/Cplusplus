a75b5715e180db8766ef4b5ef4b01e740953c147
MoonlightSn <977162804@qq.com>
2021-02-23 10:57:55 +0800

    study

---
+++ testgithub.cpp
@@ -0,0 +1,13 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生效
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char s[] = "test";
+int main() {
+    printf("Congratulasions! It works!\n");
+    printf("%d\n", strlen(s));
+    system("pause");
+    return 0;
+}
